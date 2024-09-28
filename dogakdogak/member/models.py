22b3ab67b1347b55839d8e98a3472a057b8af21d
honggyu1233@naver.com <honggyu1233@naver.com>
2024-09-28 15:21:00 +0900

    add CustomUser

---
+++ dogakdogak/member/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class CustomUser(models.Model):
+    REQUIRED_FIELDS=[]
+    user_id=models.CharField(max_length=20)
+    password=models.CharField(max_length=20)
+    name=models.CharField(max_length=20)
+    gender=models.CharFIeld(max_legnth=10)
+    age=models.IntegerField()
+    region=models.CharField(max_length=20)
+    email=models.CharField(max_length=30)
+
