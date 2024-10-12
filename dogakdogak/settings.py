037f9a920397f0956f625e2bdd64c2f4991d3e40
Oesikgogi <leecheol012566@gmail.com>
2024-10-12 16:14:42 +0900

    fix: Auth_User_Model

--- dogakdogak/settings.py
+++ dogakdogak/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'member.User'
+AUTH_USER_MODEL = 'member.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
