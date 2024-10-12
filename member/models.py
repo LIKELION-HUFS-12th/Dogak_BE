d338000c950fa8a3b2d4aaff0191bd535d6deca6
Oesikgogi <leecheol012566@gmail.com>
2024-10-12 16:06:55 +0900

    change: User to CustomUser

--- member/models.py
+++ member/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     userid = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=20)
     gender = models.CharField(max_length=10)
--- member/serializers.py
+++ member/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
-from .models import User
+from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'password', 'userid', 'name', 'gender', 'age', 'region', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])  # 비밀번호 해싱
+        user = CustomUser(**validated_data)
+        user.set_password(validated_data['password'])
         user.save()
         return user
