8c40dae0f5a0ce42b5453f79e81357d85bb0dd15
honggyu1233@naver.com <honggyu1233@naver.com>
2024-10-30 22:05:29 +0900

    change settings.py allowed_host

--- dogakdogak/settings.py
+++ dogakdogak/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['52.78.94.208','dogakdogak.store','127.0.0.1']
+ALLOWED_HOSTS = ['13.209.200.247','dogakdogak.store','127.0.0.1']
 
 
 # Application definition
