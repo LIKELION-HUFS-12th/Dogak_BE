33078d2c9094349d5436d68dfa1456f70b507184
Oesikgogi <leecheol012566@gmail.com>
2025-01-18 00:03:15 +0900

    add: register token

--- books/urls.py
+++ books/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('book_search/<str:book_name>/',book_title_search), # 책 제목 검색
-
-
-
+    path('book_search/<str:book_name>/',book_title_search),
 ]
--- member/views.py
+++ member/views.py
@@ -19,6 +19,29 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
 
+    def create(self, request, *args, **kwargs):
+        # 사용자 생성
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()  # 사용자 저장
+        
+        # 토큰 생성
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        # 응답 데이터 구성
+        response_data = {
+            "code": status.HTTP_201_CREATED,
+            "msg": "User registered successfully.",
+            "data": serializer.data,
+            "tokens": {
+                "access": access_token,
+                "refresh": refresh_token
+            }
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED)
+
 class LoginView(TokenObtainPairView):
     permission_classes = [permissions.AllowAny]
 
@@ -34,9 +57,6 @@ class LogoutView(APIView):
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         
-
-
-
         
 @api_view(['GET'])
 def profileView(request, userid_pk):
