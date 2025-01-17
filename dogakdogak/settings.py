219d20635a30d9c92d27ac2aedaccc0fec3fb367
Oesikgogi <leecheol012566@gmail.com>
2025-01-17 19:50:24 +0900

    change settings.py

--- dogakdogak/settings.py
+++ dogakdogak/settings.py
@@ -88,25 +88,26 @@ WSGI_APPLICATION = 'dogakdogak.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': 3306,
-#         # 'OPTIONS': {
-#         #     'ssl_disabled': True,  # SSL 인증 비활성화
-#         # },
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': 3306,
+        # 'OPTIONS': {
+        #     'ssl_disabled': True,  # SSL 인증 비활성화
+        # },
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
