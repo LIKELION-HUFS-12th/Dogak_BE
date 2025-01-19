7261e272252a70fd9db096e66b430b89c93c7577
Oesikgogi <leecheol012566@gmail.com>
2025-01-19 20:36:21 +0900

    Add: image url

--- books/serializers.py
+++ books/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import *
-
+from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    # SerializerMethodField를 사용해 image_url 필드를 추가
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
-        model=Book
-        fields = '__all__'  
+        model = Book
+        fields = '__all__'  # 모든 필드 포함
+
+    def get_image_url(self, obj):
+        """
+        ISBN을 기반으로 이미지 URL 생성
+        """
+        return f'https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/{obj.isbn}.jpg'
